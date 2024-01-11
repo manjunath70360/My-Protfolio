19d3e57581724761ce1a378fbbd74992c3bfecc7
Manjunath <122592837+manjunath70360@users.noreply.github.com>
2024-01-11 19:25:23 +0530

    Update index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,25 +1,32 @@
+import './index.css'
 
-import "./index.css"
-
-
-const Projects=(props) => {
+const Projects = props => {
   const {details} = props
-  const {projectTitle,imgUrl,description,technologiesUsed, projectLink} = details
-    return(
-     
-
-       <div className="project-container">
-        <img src={imgUrl} className="project-img" alt="project"/>
-        <div className="project-details">
-          <h1 className="project-name">{projectTitle}</h1>
-          <p className="para">{description}</p>
-          <p className="para"><span className="side-text">Technologies Used:</span> {technologiesUsed}</p>
-          <a href={projectLink} target="_blank" rel="noreferrer"><button type="button" className="view-btn">View Project</button></a>
-          </div>
-       </div>
- 
-    )
-  }
-
+  const {projectTitle, imgUrl, description, technologiesUsed, projectLink} =
+    details
+  return (
+    <div className="project-container">
+      <img src={imgUrl} className="project-img" alt="project" />
+      <div className="project-details">
+        <h1 className="project-name">{projectTitle}</h1>
+        <p className="para">{description}</p>
+        <p className="para">
+          <span className="side-text">Technologies Used:</span>{' '}
+          {technologiesUsed}
+        </p>
+        <a
+          href={projectLink}
+          className="btn-cont"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button" className="view-btn">
+            View Project
+          </button>
+        </a>
+      </div>
+    </div>
+  )
+}
 
 export default Projects
