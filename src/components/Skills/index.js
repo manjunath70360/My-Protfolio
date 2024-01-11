44c9c04c99cbacfa32dd640816b43a8197503c28
Manjunath <122592837+manjunath70360@users.noreply.github.com>
2024-01-11 19:26:12 +0530

    Update index.js

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,15 +1,15 @@
-import "./index.css"
+import './index.css'
 
-const Skills = (props)=>{
-    const {skillDetails} = props
-    const {technology,techUrl} = skillDetails
+const Skills = props => {
+  const {skillDetails} = props
+  const {technology, techUrl} = skillDetails
 
-    return(
-<li className="skill-item">
-<img src={techUrl} className="tech-icon"/>
-<p className="tech-name">{technology}</p>
-</li>
-    )
+  return (
+    <li className="skill-item">
+      <img src={techUrl} className="tech-icon" alt="icon" />
+      <p className="tech-name">{technology}</p>
+    </li>
+  )
 }
 
 export default Skills
